0edb730e4e84341a05d13d31f59fe6640c6a4169
Artem Konovalov <ak@gncn.ru>
2019-03-07 16:34:11 +0300

    Modified LargestPrimeFactor.py

--- LargestPrimeFactor.py
+++ LargestPrimeFactor.py
@@ -1,3 +1,6 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 import math
 
 number = 600851475143
@@ -8,7 +11,7 @@ for i in range(1, math.ceil(math.sqrt(number))):
     if number % i == 0:
         factorsList.append(int(i))
         factorsList.append(int(number / i))
-factorsList.sort()
+factorsList.sort(reverse=True)
 
 # pop 1 and "number"
 factorsList.pop(0)
@@ -25,6 +28,6 @@ for factor in factorsList:
             break
         i += 1
     if not isComposite:
+        print(factor)
         primeFactorsList.append(factor)
-
-print(primeFactorsList[-1])
+        break
